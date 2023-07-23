1d9ac7e90bc71306163db45a082ef55bd3b7edaa
ookuboc5399 <ankle0614@gmail.com>
2023-07-23 20:51:44 +0900

    second-commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -108,8 +107,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
